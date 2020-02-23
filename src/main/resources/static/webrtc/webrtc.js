159cc12e3f520bbb3d029d7acc9ad30dcc329f00
jihoon-jeon <jihoon-jeon.0728@navercorp.com>
2020-02-23 19:08:08 +0900

    compile

--- src/main/resources/static/webrtc/webrtc.js
+++ src/main/resources/static/webrtc/webrtc.js
@@ -2,112 +2,101 @@
 
 class WebRTC {
   constructor(url) {
-    this.configuration = {
+    const configuration = {
       'iceServers': [
-        {'urls': 'stun:stun.l.google.com:19302'}
+        {
+          'urls': [
+            'stun:stun.l.google.com:19302',
+            'stun:stun1.l.google.com:19302',
+            'stun:stun2.l.google.com:19302',
+            'stun:stun3.l.google.com:19302',
+            'stun:stun4.l.google.com:19302'
+          ]
+        }
       ]
     };
-    this.peerConnection = new RTCPeerConnection(this.configuration);
+    this.offerOptions = {
+      iceRestart: true,
+      offerToReceiveAudio: true,
+      offerToReceiveVideo: true
+    };
+    this.peerConnection = new RTCPeerConnection(configuration);
     this.signalingSock = new SockJS(url);
-    this.iceCandidate = [];
+    this.dataChannel = this.peerConnection.createDataChannel('test');
+  }
+
+  initStream() {
+    navigator.mediaDevices.getUserMedia({'video': true, 'audio': true})
+             .then(stream => {
+               document.querySelector('#localVideo').srcObject = stream;
+               stream.getTracks().forEach(track => {
+                 peerConnection.addTrack(track, stream);
+               });
+             });
   }
 
   initPeerConnection(ondata) {
-    const peerConnection = this.peerConnection;
-    const iceCandidate = this.iceCandidate;
     const signalingSock = this.signalingSock;
 
     this.peerConnection.onicecandidate = async event => {
-      console.log('on ice candidate:', event);
       if (event.candidate) {
-        iceCandidate.push(event.candidate);
         signalingSock.send(JSON.stringify({'iceCandidate': event.candidate}));
       }
     };
-    this.peerConnection.onconnectionstatechange = async event => {
-      console.log('on connection state changed');
-    };
     this.peerConnection.ondatachannel = async event => {
       console.log('on data channel');
-      this.dataChannel = event.channel;
-      this.dataChannel.addEventListener('message', ondata);
-    };
-    this.peerConnection.oniceconnectionstatechange = async event => {
-      console.log('oniceconnectionstatechange', event)
+      const receivingDataChannel = event.channel;
+      receivingDataChannel.addEventListener('message', ondata);
     };
 
     const remoteStream = new MediaStream();
+    const remoteVideo = document.querySelector('#remoteVideo');
+    remoteVideo.srcObject = remoteStream;
+
     this.peerConnection.ontrack = async event => {
-      document.querySelector('#remoteVideo').srcObject = remoteStream;
-      peerConnection.addTrack(event.track, remoteStream);
+      remoteStream.addTrack(event.track);
     };
   }
 
-  initSignal(call) {
+  initSignal(isCaller) {
     const signalingSock = this.signalingSock;
     const peerConnection = this.peerConnection;
-    const iceCandidate = this.iceCandidate;
+    const offerOptions = this.offerOptions;
     signalingSock.onopen = async function () {
       console.log('signaling connected');
       signalingSock.onmessage = async event => {
         const message = JSON.parse(event.data);
         console.log('signaling message: ', message);
+
         // WebRTC 연결 요청 받은 경우
         if (message.offer) {
           await peerConnection.setRemoteDescription(new RTCSessionDescription(message.offer));
-          const answer = await peerConnection.createAnswer();
-          peerConnection.setLocalDescription(answer)
-                        .then(() => {
-                          signalingSock.send(JSON.stringify({'answer': answer}));
-                          if (iceCandidate.length > 0) {
-                            signalingSock.send(JSON.stringify({'iceCandidates': iceCandidate}));
-                          }
-                        });
+          const answer = await peerConnection.createAnswer(offerOptions);
+          await peerConnection.setLocalDescription(answer);
+          signalingSock.send(JSON.stringify({'answer': answer}));
         }
+
         // WebRTC 연결 응답 받은 경우
         if (message.answer) {
           const remoteDesc = new RTCSessionDescription(message.answer);
           await peerConnection.setRemoteDescription(remoteDesc);
         }
+
         // ICE Candidate 요청을 받은 경우
         if (message.iceCandidate) {
-          console.log('ice', message.iceCandidate);
           peerConnection.addIceCandidate(message.iceCandidate)
+                        .then(() => console.log('iceCandidate added'))
                         .catch(e => {
                           console.error('Error adding received ice candidate', e);
                         });
         }
-        // ICE Candidate 요청을 받은 경우
-        if (message.iceCandidates) {
-          console.log('ice', message.iceCandidates);
-          const length = message.iceCandidates.length;
-          for (let i = 0; i < length; i++) {
-            peerConnection.addIceCandidate(message.iceCandidates[i])
-                          .catch(e => {
-                            console.error('Error adding received ice candidate', e);
-                          });
-          }
-        }
       };
 
-      if (call) {
-        const offer = await peerConnection.createOffer();
+      if (isCaller) {
+        const offer = await peerConnection.createOffer(offerOptions);
         await peerConnection.setLocalDescription(offer);
         signalingSock.send(JSON.stringify({'offer': offer}));
       }
     };
-  }
-
-  play() {
-    const peerConnection = this.peerConnection;
-    navigator.mediaDevices
-             .getUserMedia({'video': true, 'audio': true})
-             .then(videoStream => {
-               document.querySelector('#localVideo').srcObject = videoStream;
-               videoStream.getTracks()
-                          .forEach(function (track) {
-                            peerConnection.addTrack(track, videoStream);
-                          });
-             });
   }
 }
